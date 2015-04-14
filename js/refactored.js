eaca40235097d142a3bcaba140da5a7ed3ed63cc
Aishwarya <aishwarya923@gmail.com>
2015-04-14 14:44:17 -0400

    Drawing borders.

--- js/refactored.js
+++ js/refactored.js
@@ -2,10 +2,14 @@ window.onload = init;
 
 function init(){
 	var ctx = initializeCanvas();
-	
+	var state = {
+		dots: []
+	}
+
 	function tick(){
+		clear(ctx);
 		draw(ctx);
-		window.requestAnimationFrame(tick);
+		// window.requestAnimationFrame(tick);
 	}
 	window.requestAnimationFrame(tick);
 }
@@ -17,6 +21,41 @@ function initializeCanvas(){
 	return canvas.getContext('2d');
 }
 
+function clear(ctx){
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
 function draw(ctx){
 	drawBorder(ctx);
+}
+
+function drawBorder(ctx){
+	for(var i=0; i<config.GRID.length-1; i++){
+		for(var j=0; j<config.GRID[i].length-1; j++){
+			// If the element to the right is not the same, draw a line to the right
+			if(config.GRID[i][j] != config.GRID[i][j+1]){
+				drawLine(ctx, j*config.BOX_WIDTH, i*config.BOX_WIDTH, true)
+			}
+			// If the element below is not the same, draw a line below it
+			if(config.GRID[i][j]!= config.GRID[i+1][j]){
+				drawLine(ctx, j*config.BOX_WIDTH, i*config.BOX_WIDTH, false)
+			}
+		}
+	}
+}
+
+function drawLine(ctx, xPosition, yPosition, isVertical){
+	ctx.strokeStyle = '#0033ff';
+	ctx.beginPath();
+
+	if(isVertical){
+		ctx.moveTo(xPosition+config.BOX_WIDTH, yPosition);
+		ctx.lineTo(xPosition+config.BOX_WIDTH, yPosition+config.BOX_HEIGHT);	
+	}
+	else{
+		ctx.moveTo(xPosition, yPosition+config.BOX_HEIGHT);
+		ctx.lineTo(xPosition+config.BOX_WIDTH, yPosition+config.BOX_HEIGHT);
+	}
+
+	ctx.stroke();
 }
